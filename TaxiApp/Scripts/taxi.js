e51de7daaa8f868cc02fde6333402a62a9287b9f
Matt Roberts <roberts.mattroberts@gmail.com>
2012-07-26 22:00:24 +0100

    Made some changes, but no 100% map yet

--- TaxiApp/Scripts/taxi.js
+++ TaxiApp/Scripts/taxi.js
@@ -1,4 +1,14 @@
 ﻿// Map init here please
 $(function () {
-    alert("hello");
+
+    var map = new OpenLayers.Map("map");
+    var mapnik = new OpenLayers.Layer.OSM();
+    var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+    var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+    var position = new OpenLayers.LonLat(-2.800784, 54.048354).transform(fromProjection, toProjection);
+    var zoom = 15;
+
+    map.addLayer(mapnik);
+    map.setCenter(position, zoom);
+
 });
